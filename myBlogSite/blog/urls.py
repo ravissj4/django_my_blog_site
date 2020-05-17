12ef959ad9be5399e1ba291542ba70640cd5945a
RAVI RANJAN <raviranjan@cse.iitb.ac.in>
2020-05-17 18:37:12 +0530

    added url linking for the deletetion of posts in urls.py

--- myBlogSite/blog/urls.py
+++ myBlogSite/blog/urls.py
@@ -9,4 +9,5 @@ url_patterns = [
     url(r'^post/new/$', views.PostCreateView.as_view(), name='post_new'),
     url(r'^post/(?P<pk>\d+)/edit/$', views.PostUpdateView.as_view(), name='post_edit'),
     url(r'^drafts/$', views.DraftListView.as_view(), name='post_drafts_list'),
+    url(r'^post/(?P<pk>\d+)/remove/$', views.PostDeleteView.as_view(), name='post_remove'),
 ]
